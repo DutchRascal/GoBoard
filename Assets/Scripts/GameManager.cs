ffdee7b0d502de32fff5fe1549997b18fbdd617b
DutchRascal <DutchRascal@fritz.box>
2020-03-28 15:06:40 +0100

    GB-026

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Board m_board;
+    PlayerManager m_player;
+    bool
+        m_hasLevelStarted = false,
+        m_isGamePlaying = false,
+        m_isGameOver = false,
+        m_hasLevelFinished = false;
+
+    public bool HasLevelStarted { get => m_hasLevelStarted; set => m_hasLevelStarted = value; }
+    public bool IsGamePlaying { get => m_isGamePlaying; set => m_isGamePlaying = value; }
+    public bool IsGameOver { get => m_isGameOver; set => m_isGameOver = value; }
+    public bool HasLevelFinished { get => m_hasLevelFinished; set => m_hasLevelFinished = value; }
+
+    public float delay = 1f;
+
+    private void Awake()
+    {
+        m_board = Object.FindObjectOfType<Board>().GetComponent<Board>();
+        m_player = Object.FindObjectOfType<PlayerManager>().GetComponent<PlayerManager>();
+    }
+
+    private void Start()
+    {
+        if (m_player && m_board)
+        {
+            StartCoroutine(RunGameLoop());
+        }
+        else
+        {
+            Debug.LogWarning("GAMEMANAGER Error: no player or board found");
+        }
+    }
+
+    IEnumerator RunGameLoop()
+    {
+        yield return StartCoroutine(StartLevelRoutine());
+        yield return StartCoroutine(PlayLevelRoutine());
+        yield return StartCoroutine(EndLevelRoutine());
+    }
+
+    IEnumerator EndLevelRoutine()
+    {
+        yield return null;
+    }
+
+    IEnumerator PlayLevelRoutine()
+    {
+        yield return null;
+    }
+
+    IEnumerator StartLevelRoutine()
+    {
+        yield return null;
+    }
+}
